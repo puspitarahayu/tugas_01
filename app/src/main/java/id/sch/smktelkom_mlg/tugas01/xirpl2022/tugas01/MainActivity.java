98822a707325c54960a18ba89246db2fd53160b6
Puspita <Rahayupuspita571@gmail.com>
2016-09-18 06:17:02 +0700

    memberikan fungsi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2022/tugas01/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2022/tugas01/MainActivity.java
@@ -1,16 +1,21 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl2022.tugas01;
 
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
     EditText etNama;
     EditText etTelp;
@@ -21,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
     Button bOrder;
     Spinner spDomisili;
     TextView tvHasil
+    String Nama = etNama.getText().toString();
+    int Telepon = Integer.parseInt(etTelp.getText().toString());
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,17 +64,99 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.spinnerDomisili);
         tvHasil2 = (TextView)
                 findViewById(R.id.textViewHasil2);
-        tvHasil3= (TextView)
+        tvHasil3 = (TextView)
                 findViewById(R.id.textViewHasil3);
 
         bOrder.setOnClickListener(new view.OnClickListener()
         @Override
-        public void OnClick(View view)
+        public void OnClick (View view)
         {
             doProcess();
             doClick();
-        });
-
-
+        })
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://id.sch.smktelkom_mlg.tugas01.xirpl2022.tugas01/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://id.sch.smktelkom_mlg.tugas01.xirpl2022.tugas01/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    tvHasil.setText(Nama+" Pesanan Anda segera kami proses, trimakasih sudah membeli produk kami, kami tunggu pemesanan selanjutnya oleh  "+Telepon)
+
 }
+}
+
+private boolean isValid()
+        {
+        boolean valid=true;
+
+        String Nama=etNama.getText().toString();
+        String Telepon=etTelp.getText().toString();
+
+        if(Nama.isEmpty())
+        {
+        etNama.setError("Nama Belum Diisi");
+        valid=false;
+        }
+        else if(etNama/length()<3)
+        }
+        etNama.setError("Nama minimal 3 karakter");
+        valid=false;
+        }
+
+        else
+        {
+        etNama.setError(null);
+        }
+        if(Telepon.isEmpty())
+        {
+        etTelp.setError("Telepon Belum Diisi");
+        valid=false;
+        }
+
+        else
+        {
+        etTelp.setError(null);
+        }
+        return valid;
+        }
+        }
+
